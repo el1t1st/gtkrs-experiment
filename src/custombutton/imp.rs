72bebc2000f71eda1d01e27609f31779e9807ce1
Alex K <kleydints@petalmail.com>
2023-02-25 20:10:33 +0100

    the value shown in the value is stale, it is not updated

--- src/custombutton/imp.rs
+++
@@ -1,6 +0,0 @@
-use gtk::glib;
-use gtk::subclass::prelude::*;
-
-// Object holding the state
-#[derive(Default)]
-pub struct CustomButton:
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,44 @@
-fn main() {}
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::Application;
+use std::cell::Cell;
+use std::rc::Rc;
+
+static APP_ID: &str = "org.gtkrs-experiment";
+
+fn main() -> glib::ExitCode {
+    let app = gtk::Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(application: &Application) {
+    let number = Rc::new(Cell::new(0));
+
+    let number_inc = number.clone();
+    let button_inc = gtk::Button::builder().label("Plus 1").build();
+
+    button_inc.connect_clicked(move |_| {
+        number_inc.set(number_inc.get() + 1);
+        println!("The number_inc: {}", number_inc.get());
+    });
+
+    let label_info = gtk::Label::builder()
+        .label(format!("The number is {}", number.get()))
+        .build();
+
+    let vbox = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .build();
+
+    vbox.append(&label_info);
+    vbox.append(&button_inc);
+
+    let window = gtk::ApplicationWindow::builder()
+        .application(application)
+        .title("GTK Counter Experiment")
+        .child(&vbox)
+        .build();
+
+    window.present()
+}
